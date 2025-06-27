38f66993172b68ad2bff9126493e5486886526bb
narayana-pmt20 <72030924+narayana-pmt20@users.noreply.github.com>
2025-06-27 13:50:15 +0530

    Delete actions directory

--- actions/ssoActions.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE_API_URL = '/api/v2';
-
-export async function getSSOURL(type, value) {
-  let json = await axios.get(`${BASE_API_URL}/sso/${type}.json?value=${value}`, {
-  })
-  return json.data.url;
-}
